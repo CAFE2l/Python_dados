73c271ca15379903d99926e11f0899dafe355dfa
Your Name <you@example.com>
2026-07-10 14:26:29 -0300

    first commit

---
+++ bagacas/bagaca.py
@@ -0,0 +1,16 @@
+# using while as a loop
+
+# contador = 1
+# while contador <= 3:
+# nota_1 = float(input("Digite sua primeira nota: "))
+# nota_2 = float(input("Digite sua segunda nota: "))
+# print(f"Media: {(nota_1 + nota_2) / 2}")
+# contador += 1
+
+
+# USING FOR AS A LOOP
+for contador in range(1, 4):
+    nota_1 = float(input("Digite sua primeira nota: "))
+    nota_2 = float(input("Digite sua segunda nota: "))
+
+    print(f"Media: {(nota_1 + nota_2) / 2}")
---
+++ lixos/lixo1.py
@@ -0,0 +1,56 @@
+cores = {
+    "limpa": "\033[m",
+    "vermelho": "\033[31m",
+    "verde": "\033[32m",
+    "amarelo": "\033[33m",
+    "azul": "\033[34m",
+    "roxo": "\033[35m",
+    "ciano": "\033[36m",
+    "cinza": "\033[37m",
+    "pretoebranco": "\033[7;30m",
+}
+
+fundo = {
+    "branco": "\033[m",
+    "vermelho": "\033[41m",
+    "verde": "\033[42m",
+    "amarelo": "\033[43m",
+    "azul": "\033[44m",
+    "roxo": "\033[45m",
+    "ciano": "\033[46m",
+    "cinza": "\033[47m",
+}
+
+estilos = {
+    "reset": "\033[0m",
+    "negrito": "\033[1m",
+    "fraco": "\033[2m",
+    "italico": "\033[3m",
+    "sublinhado": "\033[4m",
+    "inverso": "\033[7m",
+    "invisivel": "\033[8m",
+    "tachado": "\033[9m",
+}
+frase = "numeros dentro de um periodo"
+print(
+    f"{cores['amarelo']}{estilos['negrito']}{'==' * 10 + '='}{cores['verde']}FATORIAL{cores['limpa']}{cores['amarelo']}{estilos['negrito']}{'==' * 11}{cores['limpa']}"
+)
+print(
+    f"{cores['vermelho']}{estilos['italico']}{estilos['negrito']}{frase.center(50)}{estilos['reset']}"
+)
+print(f"{cores['amarelo']}{estilos['negrito']}{'===' * 17}{cores['limpa']}")
+num1 = int(input("Digite 1 numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+if num1 < num2:
+    print(f"Os numeros que estao no periodo de {num1} e {num2} e de : ")
+    while num1 < num2:
+        num1 += 1
+        print(num1 - 1)
+
+
+elif num1 > num2:
+    print(f"O numeros que estao no periodo de {num1} e {num2} e de: ")
+    while num1 > num2:
+        num2 += 1
+        print(num2 - 1)
