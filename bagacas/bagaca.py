e6fd4d54da8a8f60bc9928f1b5ed642bdc28d172
Your Name <you@example.com>
2026-07-10 16:52:55 -0300

    adding the new features

--- bagacas/bagaca.py
+++ bagacas/bagaca.py
@@ -1,16 +1,25 @@
-# using while as a loop
+# # using while as a loop
 
-# contador = 1
-# while contador <= 3:
-# nota_1 = float(input("Digite sua primeira nota: "))
-# nota_2 = float(input("Digite sua segunda nota: "))
-# print(f"Media: {(nota_1 + nota_2) / 2}")
-# contador += 1
+# # contador = 1
+# # while contador <= 3:
+# # nota_1 = float(input("Digite sua primeira nota: "))
+# # nota_2 = float(input("Digite sua segunda nota: "))
+# # print(f"Media: {(nota_1 + nota_2) / 2}")
+# # contador += 1
 
 
-# USING FOR AS A LOOP
-for contador in range(1, 4):
-    nota_1 = float(input("Digite sua primeira nota: "))
-    nota_2 = float(input("Digite sua segunda nota: "))
+# # USING FOR AS A LOOP
+# for contador in range(1, 4):
+#     nota_1 = float(input("Digite sua primeira nota: "))
+#     nota_2 = float(input("Digite sua segunda nota: "))
 
-    print(f"Media: {(nota_1 + nota_2) / 2}")
+#     print(f"Media: {(nota_1 + nota_2) / 2}")
+
+# USANDO LISTAS
+lista = ["beta", 18, "CARALO", True]
+# for elemento in lista:
+#     print(elemento)
+
+
+lista.remove(True)
+print(lista)
---
+++ bagacas/bagunca.py
@@ -0,0 +1,11 @@
+estudante = {
+    "matricula": 20001121,
+    "dia_cadastro": 10 / 10 / 2010,
+    "mes_cadastro": 10,
+    "turma": "2D",
+}
+
+print(estudante["turma"])
+estudante["modalide"] = "EAD"
+
+print(estudante)
