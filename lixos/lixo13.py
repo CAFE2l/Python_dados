afe755975f1cf1a5b5bc2a04348f34be520c5550
Your Name <you@example.com>
2026-07-10 16:24:53 -0300

    test

---
+++ lixos/lixo13.py
@@ -0,0 +1,15 @@
+numero = int(input("Digite um numero pra ver se e primo: "))
+
+if numero < 2:
+    print(f"O numnero {numero} nao e primo")
+else:
+    primo = True
+    for i in range(2, numero):
+        if numero % i == 0:
+            primo = False
+            break
+
+        if numero == primo:
+            print(f"O numero {numero} e primo")
+        else:
+            print(f"O numero {numero} nao e primo")
